ddad353b2f80355b4ae03fc64d336cc0ed5d9d62
August-Brandt <augustbrandt170@gmail.com>
2024-09-16 21:59:30 +0200

    Add definition file for defining url and keyword filter

---
+++ docs/defFile.md
@@ -0,0 +1,13 @@
+# The definition file
+
+## URL
+The first line of the definition file should be the staring url
+
+## Keywords
+A keyword is defined on a new line staring the line with `key:`. The rest of the line will define the value of the keyword.
+Only listings that contain one or more of the defined keywords in the title will be valid.
+
+### Example
+```
+key:rtx 4070
+```
--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
 import utils
+import sys
 
-url = "https://www.dba.dk/computer-og-spillekonsoller/hardware-og-software/grafikkort/side-1"
+args = sys.argv
+if len(args) < 2:
+    print("Usage: python3 main.py [path-to-definition-file]")
+    quit()
+
+# Retrieve url from definition file
+keywords = []
+with open(args[1], "r") as f:
+    url = f.readline().rstrip()
+    while line := f.readline().rstrip():
+        match line[0:4]:
+            case "key:":
+                keywords.append(line[4:].lower())
+            case _:
+                print(f"Invalid syntax: {line[0:4]}\n\tLine: {line}")
+                quit()
+
+print("Getting listing from: " + url + " ...")
 
 pages = utils.GetNumberPages(url)
 print("Pages:", pages)
@@ -9,12 +27,14 @@ listings = utils.GetAllListings(url, pages)
 
 print("Total listings:", len(listings))
 
-# print("-"*45)
-# for listing in listings:
-#     print(listing[0])
-#     print("\t" + listing[1])
-#     print("-"*45)
+
+print("-"*45)
+for listing in listings:
+    for keyword in keywords:
+        if keyword in listing[0].lower():
+            print(listing[0])
+            print("\t" + listing[1])
+            print("-"*45)
 
 # with open('test.html', "wb") as f:
 #     f.write(response.content)
-
